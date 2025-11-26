6993bd0a3cf49f0fe7411dc8aa99fe00ca77e3b8
Jona-HN <msjona28@outlook.com>
2025-11-26 14:39:44 -0800

    Initial commit

---
+++ src/test/java/com/uabc/jonahn/springweb/LoanRequest/LoanRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uabc.jonahn.springweb.LoanRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
